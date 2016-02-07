8c0a649be2926367fa1867682fdfad40f096de72
Dennis Hernández <dennishernandezvargas@gmail.com>
2016-02-06 22:52:09 -0600

    Fix #1956
    Added customSearch options
    Example: http://jsfiddle.net/djhvscf/e3nk137y/4594/

--- src/bootstrap-table.js
+++ src/bootstrap-table.js
@@ -337,6 +337,8 @@
             detailClose: 'glyphicon-minus icon-minus'
         },
 
+        customSearch: $.noop,
+
         rowStyle: function (row, index) {
             return {};
         },
@@ -1131,6 +1133,10 @@
         var that = this;
 
         if (this.options.sidePagination !== 'server') {
+            if (this.options.customSearch !== $.noop) {
+                this.options.customSearch.apply(this, [this.searchText]);
+                return;
+            }
             var s = this.searchText && this.searchText.toLowerCase();
             var f = $.isEmptyObject(this.filterColumns) ? null : this.filterColumns;
 
